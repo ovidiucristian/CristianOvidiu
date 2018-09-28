c40cfc3be1983ae74b5475c60ce6b3b8be58acf0
ovidiucristian <ovidiumarian.cristian@gmail.ccom>
2018-09-28 23:34:56 +0300

    Merge origin/master

--- Java9OvidiuCristian/src/autoservice/Autoservice.java
+++ Java9OvidiuCristian/src/autoservice/Autoservice.java
@@ -3,40 +3,36 @@
  */
 package autoservice;
 
-import autoservice.car.Car;
-import static javafx.scene.paint.Color.color;
-import static javafx.scene.paint.Color.color;
-
 /**
  *
  * @author Ovidiu
  */
 public class Autoservice {
 
-    static car m1, m2, m3;
+    static Car m1, m2, m3;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //5.a Create 3 instances of cars having different names, colors, speed, renting price and sale price
-        // Create first car
-        car m1 = new car ("Mercedes", (short) 100 , car.Color.Yellow);
+        //5.a Create 3 instances of Cars having different names, colors, speed, renting price and sale price
+        // Create first Car
+        m1 = new Car("Mercedes", (short) 100 , Car.Color.YELLOW);
         m1.setRentPrice(100);
         m1.setSalePrice(15000);
 
-        // Create second car
-        car m2 = new car("Logan", (short) 80, Car.Color.Blue);
+        // Create second Car
+        m2 = new Car("Logan", (short) 80, Car.Color.BLUE);
         m2.setRentPrice(80);
         m2.setSalePrice(9000);
 
-        // Create third car
-        car m3 = new car("Renault", (short) 80, Car.Color.BLACK);
+        // Create third Car
+        m3 = new Car("Renault", (short) 80, Car.Color.BLACK);
         m3.setRentPrice(90);
         m3.setSalePrice(11000);
 
         /*
-             5.b. Create a private method in Autoservice, methid named "void displayAutoservice()" that display all information about all cars in format:
+             5.b. Create a private method in Autoservice, methid named "void displayAutoservice()" that display all information about all Cars in format:
 
                 Car name: ......
                 Car color: ......
@@ -44,28 +40,29 @@ public class Autoservice {
                 Car renting price: ....
                 Car sale price: ...... 
          */
-        //5.c Display all informations about all cars calling the method displayAutoservice()
-        //5.d Increase speed of first car with 10
-        m1.increaseSpeed(m1.getSpeed() + 10);
-        //5.e Decrease speed of seco nd car with 10
-        m2.decreaseSpeed(m2.getSpeed - 10);
-        //5.f Increase renting price of third car with 15 and decrease its sale price with 1200
+        //5.c Display all informations about all Cars calling the method displayAutoservice()
+        //5.d Increase speed of first Car with 10
+        m1.increaseSpeed((short) (m1.getSpeed() + 10));
+        //5.e Decrease speed of seco nd Car with 10
+        m2.decreaseSpeed((short) (m2.getSpeed() - 10));
+        //5.f Increase renting price of third Car with 15 and decrease its sale price with 1200
 
-        //5.g Display all informations about all cars calling the method displayAutoservice()
+        //5.g Display all informations about all Cars calling the method displayAutoservice()
+        displayAutoservice();
     }
 
     public Autoservice() {
     }
 
-    public static void setM1(car m1) {
+    public static void setM1(Car m1) {
         Autoservice.m1 = m1;
     }
 
-    public static void setM2(car m2) {
+    public static void setM2(Car m2) {
         Autoservice.m2 = m2;
     }
 
-    public static void setM3(car m3) {
+    public static void setM3(Car m3) {
         Autoservice.m3 = m3;
     }
 
@@ -74,21 +71,21 @@ public class Autoservice {
      */
     private static void displayAutoservice() {
         System.out.println("----------Autoservice----------");
-        System.out.println("First car");
+        System.out.println("First Car");
         System.out.println("Car name" + m1.getName());
         System.out.println("Car speed" + m1.getSpeed());
         System.out.println("Car color" + m1.getColor());
         System.out.println("Rentind price" + m1.getDailyRentPrice());
         System.out.println("Sale Price" + m1.getSalePrice());
 
-        System.out.println("Second car");
+        System.out.println("Second Car");
         System.out.println("Car name" + m2.getName());
         System.out.println("Car speed" + m2.getSpeed());
         System.out.println("Car color" + m2.getColor());
         System.out.println("Rentind price" + m2.getDailyRentPrice());
         System.out.println("Sale Price" + m2.getSalePrice());
 
-        System.out.println("Second car");
+        System.out.println("Second Car");
         System.out.println("Car name" + m3.getName());
         System.out.println("Car speed" + m3.getSpeed());
         System.out.println("Car color" + m3.getColor());
---
+++ Java9OvidiuCristian/src/autoservice/Car.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package autoservice;
+
+/**
+ *
+ * @author Ovidiu
+ */
+public class Car implements Saleable, Rentable{
+ /**
+ @enum enumeration of accepted car colors
+ */
+        public static enum Color { WHITE, RED, YELLOW, GREEN, GRAY, BLACK, BLUE };
+ /**
+ Internal hidden fields / attributes
+ */
+ private Color color;
+ private String name;
+ private short speed;
+
+        public int getRentPrice() {
+            return rentPrice;
+        }
+
+        public static boolean isRENTABLE() {
+            return RENTABLE;
+        }
+
+        public static boolean isSALEABLE() {
+            return SALEABLE;
+        }
+ private int salePrice;
+
+        public void setSalePrice(int salePrice) {
+            this.salePrice = salePrice;
+        }
+
+        public void setRentPrice(int rentPrice) {
+            this.rentPrice = rentPrice;
+        }
+ private int rentPrice;
+ /**
+ Constructors
+ */
+ public Car(){
+ name = "Default car name";
+ speed = 90;
+ }
+ public Car(String carName, short carSpeed, Color carColor){
+ name = carName;
+ speed = carSpeed;
+ color = carColor;
+ }
+ /*get-ers and set-ers*/
+ public String getName(){
+ return(name);
+ }
+ public Color getColor(){
+ return(color);
+ }
+ public short getSpeed(){
+ return(speed);
+ }
+ public void setName(String newName){
+ name = newName;
+ }
+ public void setColor(Color newColor){
+ color = newColor;
+ }
+ public void setSpeed(short newSpeed){
+ speed = newSpeed;
+ }
+ /**
+ The method increase actual speed of Car until final speed specified.
+ * @param newSpeed is new speed
+ */
+ public void increaseSpeed(short newSpeed){
+ if(newSpeed > speed){
+ speed = newSpeed;
+ }
+ else if (newSpeed < speed){
+ decreaseSpeed(newSpeed);
+ }
+ }
+ /**
+ The method decrease actual speed of Car until final speed specified.
+ * @param newSpeed is new speed
+ */
+ public void decreaseSpeed(short newSpeed){
+ if(newSpeed < speed){
+ speed = newSpeed;
+ }
+ else if (newSpeed < speed){
+ increaseSpeed(newSpeed);
+ }
+ }
+ /**
+ Abstract method defined in Saleable interface implemented by class car
+ * is implemented here.
+         * @return 
+ * @override Saleable.getSalePrice() method
+ */
+      @Override
+ public int getSalePrice(){
+ return(salePrice);
+ }
+ /**
+ Abstract method defined in Rentable interface implemented by class car
+ * is implemented here.
+         * @return 
+ * @override Rentable.getDailyRentPrice() method
+ */
+        @Override
+ public int getDailyRentPrice(){
+ return(rentPrice);
+ }
+    }
