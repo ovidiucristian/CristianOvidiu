d57bd31cf6716210e0d5e485f01378a30cd2da54
ovidiucristian <ovidiumarian.cristian@gmail.ccom>
2018-10-13 19:26:34 +0300

    Delete car.java

---
+++ Java9OvidiuCristian/src/stringsandnumbers/RegionMatchesDemo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stringsandnumbers;
+
+/**
+ *
+ * @author Ovidiu
+ */
+public class RegionMatchesDemo {
+
+ public static void main(String[] args) {
+String searchMe = "Green Eggs and Ham";
+String findMe = "Eggs";
+int searchMeLength = searchMe.length();
+int findMeLength = findMe.length();
+boolean foundIt = false;
+for (int i = 0;
+i <= (searchMeLength - findMeLength);
+i++) {
+if (searchMe.regionMatches(i, findMe, 0, findMeLength)) {
+foundIt = true;
+System.out.println(searchMe.substring(i, i + findMeLength));
+break;
+}
+}
+if (!foundIt)
+System.out.println("No match found.");
+}
+
+}
